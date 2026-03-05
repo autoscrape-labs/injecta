e5883f2874c581232963c6a4ff42147615332636
Thalison Fernandes <thalissfernandes99@gmail.com>
2026-03-04 22:56:55 -0300

    test(core): add tests for Needs marker and Dependant model

---
+++ tests/test_models.py
@@ -0,0 +1,32 @@
+from injecta.models import Dependant
+
+
+def _provider() -> str:
+    return 'value'
+
+
+def _sub_provider() -> int:
+    return 42
+
+
+class TestDependant:
+    def test_creates_with_callable(self) -> None:
+        dep = Dependant(call=_provider)
+
+        assert dep.call is _provider
+        assert dep.dependencies == []
+        assert dep.param_name == ''
+        assert dep.use_cache is True
+
+    def test_stores_sub_dependencies(self) -> None:
+        sub = Dependant(call=_sub_provider, param_name='count')
+        parent = Dependant(call=_provider, dependencies=[sub])
+
+        assert len(parent.dependencies) == 1
+        assert parent.dependencies[0].call is _sub_provider
+
+    def test_custom_param_name_and_cache(self) -> None:
+        dep = Dependant(call=_provider, param_name='config', use_cache=False)
+
+        assert dep.param_name == 'config'
+        assert dep.use_cache is False
